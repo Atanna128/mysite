129c416a0139d2da10a000f2132bf3f85b945fcf
Ngoc Tu <buingoctu1998@gmail.com>
2020-08-10 23:18:18 +0700

    Django Tutorial Part 7: Sessions framework

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -22,6 +22,11 @@ def index(request):
 
     # number of genres
     num_genres = Genre.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -29,6 +34,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'book_all': book_all,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
